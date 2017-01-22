6f272d458afe965ba6d7dce0f0ecbf717235ced9
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-01-22 17:37:37 +0800

    Change-Id: I7997308253f4e1d0ac6f965f94cec531fc24d2ba Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

---
+++ src/org/hy/common/app/Param.java
@@ -0,0 +1,188 @@
+package org.hy.common.app;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.hy.common.ChangeEvent;
+import org.hy.common.ChangeListener;
+import org.hy.common.Help;
+import org.hy.common.TriggerEvent;
+
+
+
+
+
+/**
+ * 参数信息
+ *
+ * @author   ZhengWei(HY)
+ * @version  V1.0  2013-04-20
+ *           v2.0  2015-11-02  添加：是否为只读状态，当为只读状态时，this.value只能被设置一次。默认为只读状态
+ *                             添加：this.value 的数值改变事
+ */
+public class Param 
+{
+	
+	private String       name;
+	
+	private String       value;
+	
+	private String       comment;
+	
+	/** 是否为只读状态，当为只读状态时，this.value只能被设置一次。默认为只读状态 */
+	private boolean      isOnlyRead;
+	
+	/** 添加 this.value 的数值改变事件(默认不启用) */
+	private TriggerEvent changedTrigger;
+	
+	
+	
+	public Param()
+	{
+	    this(null ,null ,null);
+	}
+	
+	
+	
+	public Param(String i_Name ,String i_Value)
+	{
+	    this(i_Name ,i_Value ,null);
+	}
+	
+	
+	
+	public Param(String i_Name ,String i_Value ,String i_Comment)
+	{
+	    this.name           = i_Name;
+	    this.value          = i_Value;
+	    this.comment        = i_Comment;
+	    this.isOnlyRead     = true;
+	    this.changedTrigger = null;
+	}
+	
+	
+	
+	public String getName() 
+	{
+		return name;
+	}
+
+	
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	
+	
+	public String getValue() 
+	{
+		return value;
+	}
+
+	
+	
+	/**
+	 * 当为只读状态时，this.value只能被设置一次。默认为只读状态
+	 * 
+	 * @param value
+	 */
+	public void setValue(String i_Value)
+	{
+	    if ( this.isOnlyRead && !Help.isNull(this.value) )
+	    {
+	        throw new IllegalArgumentException("Value is only setting once. (isOnlyRead=" + this.isOnlyRead + ")");
+	    }
+	    
+	    String v_OldValue = this.value;
+		this.value        = i_Value;
+		
+		if ( this.changedTrigger != null )
+		{
+		    try
+            {
+                this.changedTrigger.trigger("valueChanged" ,new ChangeEvent<String>(this ,v_OldValue ,i_Value));
+            }
+            catch (IllegalAccessException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IllegalArgumentException e)
+            {
+                e.printStackTrace();
+            }
+            catch (InvocationTargetException e)
+            {
+                e.printStackTrace();
+            }
+		}
+	}
+
+	
+	
+	public String getComment() 
+	{
+		return comment;
+	}
+
+	
+	
+	public void setComment(String comment) 
+	{
+		this.comment = comment;
+	}
+	
+	
+	
+    /**
+     * 获取：是否为只读状态，当为只读状态时，this.value只能被设置一次。默认为只读状态
+     */
+    public boolean isOnlyRead()
+    {
+        return isOnlyRead;
+    }
+    
+
+    
+    /**
+     * 设置：是否为只读状态，当为只读状态时，this.value只能被设置一次。默认为只读状态
+     * 
+     * @param isOnlyRead 
+     */
+    public void setOnlyRead(boolean isOnlyRead)
+    {
+        this.isOnlyRead = isOnlyRead;
+    }
+    
+    
+    
+    /**
+     * 添加 this.value 的数值改变事件
+     * 
+     * 注意：当执行此方法后，只读状态自动变为可写状态
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2015-11-02
+     * @version     v1.0
+     *
+     * @param i_ChangeListener
+     */
+    public synchronized void addChangedListener(ChangeListener<String> i_ChangeListener)
+    {
+        if ( this.changedTrigger == null )
+        {
+            this.changedTrigger = new TriggerEvent(ChangeListener.class);
+        }
+        
+        this.isOnlyRead = false;
+        this.changedTrigger.addListener(i_ChangeListener);
+    }
+
+
+
+    public String toString()
+	{
+		return this.value;
+	}
+	
+}
