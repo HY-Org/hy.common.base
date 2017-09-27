04218f76396ee1b8f0b6b5761296931b17592c59
ZhengWei <ZhengWei@ZhengWei_HY>
2017-09-27 12:43:09 +0800

    1.0.112
    Signed-off-by: ZhengWei <ZhengWei@ZhengWei_HY>

--- src/org/hy/common/Help.java
+++ src/org/hy/common/Help.java
@@ -49,6 +49,7 @@ import org.hy.common.app.Param;
  *                           2. 添加：findSames (? ,String ...)支持面向对象：参与排序的属性名，可实现xxx.yyy.www(或getXxx.getYyy.getWww)全路径的比较
  *                           3. 添加：toDistinct(? ,String ...)支持面向对象：参与排序的属性名，可实现xxx.yyy.www(或getXxx.getYyy.getWww)全路径的比较
  *               2017-07-23  1. 添加：getMacs() 获取本机全部的Mac
+ *               2017-09-27  1. 添加：toLike(...)系列方法。可实现SQL语句中的like查询。
  *
  */
 public class Help
@@ -3819,6 +3820,97 @@ public class Help
      * 
      * @return                      创建一个新的集合。异常时，返回null
      */
+    public final static <T> List<T> toLike(PartitionMap<? ,T> i_Datas ,String i_SelectPropertyName ,String ... i_FindKeys)
+    {
+        return toLike(i_Datas ,i_SelectPropertyName ,false ,i_FindKeys);
+    }
+    
+    
+    
+    /**
+     * 纵向比较集合元素中的某一个属性值是否包含多个关键字，并形成新的集合。新集合与原集合结构相同。
+     * 
+     * 模拟SQL语句中的like查询
+     * 
+     *   分为两种判定标准
+     *     1. 包含全部关键字为true
+     *     2. 包含任何一个关键字为true
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-09-27
+     * @version     v1.0
+     * 
+     * @param i_Datas               集合数据
+     * @param i_SelectPropertyName  纵向比较的属性名称，不区分大小写。支持属性名称的多级获取 xxx.yyy.zzz
+     * @param i_IsAllContains       是否包含全部关键字为true
+     * @param i_FindKeys            关键字组
+     * 
+     * @return                      创建一个新的集合。异常时，返回null
+     */
+    public final static <T> List<T> toLike(PartitionMap<? ,T> i_Datas ,String i_SelectPropertyName ,boolean i_IsAllContains ,String ... i_FindKeys)
+    {
+        if ( Help.isNull(i_Datas) || Help.isNull(i_SelectPropertyName) || i_Datas.rowCount() <= 0 )
+        {
+            return null;
+        }
+        
+        T v_One = i_Datas.values().iterator().next().get(0);
+        if ( v_One == null )
+        {
+            return null;
+        }
+        
+        List<T> v_Ret = new ArrayList<T>();
+        
+        try
+        {
+            MethodReflect v_MethodReflect = new MethodReflect(v_One ,i_SelectPropertyName ,true ,MethodReflect.$NormType_Getter);
+            
+            for (List<T> v_ChildList : i_Datas.values())
+            {
+                if ( !Help.isNull(v_ChildList) )
+                {
+                    for (T v_Item : v_ChildList)
+                    {
+                        Object v_Value = v_MethodReflect.invokeForInstance(v_Item);
+                        
+                        if ( v_Value != null 
+                          && StringHelp.isContains(v_Value.toString() ,i_IsAllContains ,i_FindKeys) )
+                        {
+                            v_Ret.add(v_Item);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception exce)
+        {
+            exce.printStackTrace();
+        }
+        
+        return v_Ret;
+    }
+    
+    
+    
+    /**
+     * 纵向比较集合元素中的某一个属性值是否包含多个关键字，并形成新的集合。新集合与原集合结构相同。
+     * 
+     * 模拟SQL语句中的like查询。
+     * 
+     * (包含任何一个关键字为true)
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-09-27
+     * @version     v1.0
+     * 
+     * @param i_Datas               集合数据
+     * @param i_SelectPropertyName  纵向比较的属性名称，不区分大小写。支持属性名称的多级获取 xxx.yyy.zzz
+     * @param i_IsAllContains       是否包含全部关键字为true
+     * @param i_FindKeys            关键字组
+     * 
+     * @return                      创建一个新的集合。异常时，返回null
+     */
     public final static <T> List<T> toLike(Set<T> i_Datas ,String i_SelectPropertyName ,String ... i_FindKeys)
     {
         return toLike(i_Datas ,i_SelectPropertyName ,false ,i_FindKeys);
@@ -4108,6 +4200,87 @@ public class Help
                   && StringHelp.isContains(v_Item.toString() ,i_IsAllContains ,i_FindKeys) )
                 {
                     v_Ret.add(v_Item);
+                }
+            }
+        }
+        catch (Exception exce)
+        {
+            exce.printStackTrace();
+        }
+        
+        return v_Ret;
+    }
+    
+    
+    
+    /**
+     * 比较集合元素中的某一个属性值是否包含多个关键字，并形成新的集合。新集合与原集合结构相同。
+     * 
+     * 模拟SQL语句中的like查询
+     * 
+     *   分为两种判定标准
+     *     1. 包含全部关键字为true
+     *     2. 包含任何一个关键字为true
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-09-27
+     * @version     v1.0
+     * 
+     * @param i_Datas               集合数据
+     * @param i_IsAllContains       是否包含全部关键字为true
+     * @param i_FindKeys            关键字组
+     * 
+     * @return                      创建一个新的集合。异常时，返回null
+     */
+    public final static <T> List<T> toLike(PartitionMap<? ,T> i_Datas ,String ... i_FindKeys)
+    {
+        return toLike(i_Datas ,false ,i_FindKeys);
+    }
+    
+    
+    
+    /**
+     * 比较集合元素中的某一个属性值是否包含多个关键字，并形成新的集合。新集合与原集合结构相同。
+     * 
+     * 模拟SQL语句中的like查询
+     * 
+     *   分为两种判定标准
+     *     1. 包含全部关键字为true
+     *     2. 包含任何一个关键字为true
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-09-27
+     * @version     v1.0
+     * 
+     * @param i_Datas               集合数据
+     * @param i_IsAllContains       是否包含全部关键字为true
+     * @param i_FindKeys            关键字组
+     * 
+     * @return                      创建一个新的集合。异常时，返回null
+     */
+    public final static <T> List<T> toLike(PartitionMap<? ,T> i_Datas ,boolean i_IsAllContains ,String ... i_FindKeys)
+    {
+        if ( Help.isNull(i_Datas) || i_Datas.rowCount() <= 0 )
+        {
+            return null;
+        }
+        
+        List<T> v_Ret = new ArrayList<T>();
+        
+        try
+        {
+            for (List<T> v_ChildList : i_Datas.values())
+            {
+                if ( !Help.isNull(v_ChildList) )
+                {
+                    for (T v_Item : v_ChildList)
+                    {
+                        if ( v_Item != null 
+                          && StringHelp.isContains(v_Item.toString() ,i_IsAllContains ,i_FindKeys) )
+                        {
+                            v_Ret.add(v_Item);
+                        }
+                    }
                 }
             }
         }
