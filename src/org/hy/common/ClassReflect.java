129717b3ba4aa4797150cd32bf4687081502ad17
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-05 13:36:33 +0800

    1.0.120
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- src/org/hy/common/ClassReflect.java
+++ src/org/hy/common/ClassReflect.java
@@ -21,6 +21,7 @@ import org.hy.common.TablePartition;
  * @author      ZhengWei(HY)
  * @version     v1.0  
  * @createDate  2014-04-17
+ *              2017-12-04  1.添加：getAnnotationMethods()方法。
  */
 public final class ClassReflect
 {
@@ -63,6 +64,37 @@ public final class ClassReflect
                 v_ClassInfo.setFields(v_Fields);
                 
                 v_Ret.putRow(ElementType.FIELD ,v_ClassInfo);
+            }
+        }
+        
+        return v_Ret;
+    }
+    
+    
+    
+    /**
+     * 从指定集合i_Classes中，挑出有注解的Class的方法。
+     * 
+     * @param i_Classes          Java元类型的集合
+     * @param i_AnnotationClass  注解类型的元类型
+     * @return
+     */
+    public static List<ClassInfo> getAnnotationMethods(List<Class<?>> i_Classes ,Class<? extends Annotation> i_AnnotationClass)
+    {
+        List<ClassInfo> v_Ret = new ArrayList<ClassInfo>();
+        
+        for (int i=0; i<i_Classes.size(); i++)
+        {
+            Class<?>  v_Class     = i_Classes.get(i);
+            ClassInfo v_ClassInfo = new ClassInfo(v_Class);
+            
+            // 判断类中的自有方法是否注解
+            List<Method> v_Methods = MethodReflect.getAnnotationMethods(v_Class ,i_AnnotationClass);
+            if ( !Help.isNull(v_Methods) )
+            {
+                v_ClassInfo.setMethods(v_Methods);
+                
+                v_Ret.add(v_ClassInfo);
             }
         }
         
