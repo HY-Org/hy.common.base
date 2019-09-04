7315cebfd843328f75a9890dc18b48c1997631de
HY-ZhengWei <hy.zhengwei@qq.com>
2019-09-04 16:19:29 +0800

    1.0.148

--- Maven.Deploy.Win.Base.bat
+++ Maven.Deploy.Win.Base.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.common.base.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/base/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/base/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.base.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/base/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/base/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
