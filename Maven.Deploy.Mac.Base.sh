556c2d1e4de6c2f44ff7814f4009d54240b12f1e
ZhengWei <HY.ZhengWei@qq.com>
2022-11-02 20:10:38 +0800

    1.0.161
    1. 完善：工程外围配置及环境

--- Maven.Deploy.Mac.Base.sh
+++ Maven.Deploy.Mac.Base.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+start mvn install:install-file -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml
+start mvn install:install-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml
+
+
+mvn deploy:deploy-file         -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file         -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
--- Maven.Deploy.Win.Base.bat
+++ Maven.Deploy.Win.Base.bat
@@ -1,2 +1,5 @@
-start mvn deploy:deploy-file -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+start mvn install:install-file -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml
+start mvn install:install-file -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml
+
+start mvn deploy:deploy-file   -Dfile=hy.common.base.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+start mvn deploy:deploy-file   -Dfile=hy.common.base-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.base/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
